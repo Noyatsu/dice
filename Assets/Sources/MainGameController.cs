6ed5b00df2990bdf38fe6345f90f1a6caf2c9b19
witalosk <shun_september@live.jp>
2018-09-17 13:24:10 +0900

    [change] さいころの面がデータ上扱えるようにした

--- Assets/Sources/MainGameController.cs
+++ Assets/Sources/MainGameController.cs
@@ -2,25 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainGameController : MonoBehaviour {
+public class MainGameController : MonoBehaviour
+{
 
-	public int[,] board = new int[10, 10];
+    public int[,] board = new int[10, 10]; //!<さいころのIDを格納
+    public int[,] board_num = new int[10, 10]; //!< さいころの面を格納
 
-	// Use this for initialization
-	void Start () {
-		for (int i = 0; i < board.GetLength(0); i++)
-		{
-				for (int j = 0; j < board.GetLength(1); j++)
-				{
-						board[i, j] = -1;
-				}
-		}
-		board[0, 0] = 0;
-		board[4, 4] = 1;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        //配列の初期化
+        for (int i = 0; i < board.GetLength(0); i++)
+        {
+            for (int j = 0; j < board.GetLength(1); j++)
+            {
+                board[i, j] = -1;
+                board_num[i, j] = -1;
+            }
+        }
+
+        //以下仮
+        board[0, 0] = 0;
+        board_num[0, 0] = 1;
+        board[4, 4] = 1;
+        board_num[4, 4] = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
