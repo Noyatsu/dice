7b62a726cadc638059ad64bb8f3b0b7b0b916149
Naoboe <k.nao0626@gmail.com>
2018-09-24 03:02:00 +0900

    [change]レベルの上がり方、レベルにつれて早くなるダイス生成の間隔を調整

--- Assets/Sources/MainGameController.cs
+++ Assets/Sources/MainGameController.cs
@@ -56,7 +56,7 @@ public class MainGameController : MonoBehaviour
         objStatusText = StatusText.GetComponent<StatusTextController>();
 
         //さいころをいくつか追加
-        for (int i = 0; i < 5; i++)
+        for (int i = 0; i < 10; i++)
         {
             randomDiceGenerate();
         }
@@ -114,24 +114,20 @@ public class MainGameController : MonoBehaviour
         }
 
         timeElapsed += Time.deltaTime;
-
-        // 5秒ごとにさいころ追加
-        if (timeElapsed >= (15.0f/level))
+       
+        if(level > 20) { //現状レベル20で速さは打ち止め
+            if (timeElapsed >= 1.5)
+            {
+                randomDiceGenerate();
+                timeElapsed = 0.0f;
+            }
+        }
+        // さいころ追加 スタートは3.5秒ごと、ゴールは1.5秒ごと
+        else if (timeElapsed >= (3.5f-0.1f*level))
         {
             randomDiceGenerate();
             timeElapsed = 0.0f;
         }
-
-        //レベルの変化
-
-        level = (int)Mathf.Sqrt(score) + 1;
-        stageBefore = stage;
-        stage = level % 18 / 3 - 3;
-        if (stage < 0)
-        {
-            stage += 6; 
-        }
-
 
         //ゲームステージの設定
         if (stage != stageBefore)
@@ -420,7 +416,7 @@ public class MainGameController : MonoBehaviour
                     score += level * 100;
                     objStatusText.setText("ステージボーナス! +" + count * level * 10);
                 }
-
+                ComputeLevel(); //レベル計算
             }
         }
         else
@@ -457,6 +453,7 @@ public class MainGameController : MonoBehaviour
                     score += level * 100;
                     objStatusText.setText("ステージボーナス! +" + count* level * 10);
                 }
+                ComputeLevel(); //レベル計算
             }
         }
 
@@ -545,5 +542,32 @@ public class MainGameController : MonoBehaviour
 
             return cnt;
     }
+
+    void ComputeLevel () {
+        int a = 120; //おおよそ1レベルの上昇に必要なスコア
+        int b = a; //前の必要経験値を記録する
+        int lv = 1;
+
+        //レベルの変化
+        while (true) {
+            b = (int)((a * lv + b * 1.1)/ 2);
+            if (score > b) {
+                lv++;
+            } else {
+                break;
+            }
+        }
+
+        level = lv;
+        stageBefore = stage;
+        stage = level % 18 / 3 - 3;
+        if (stage < 0)
+        {
+            stage += 6;
+        }
+
+    }
+
+
 }
 
