21323176321c1539651462aae5139887b5389a80
witalosk <shun_september@live.jp>
2018-09-26 02:56:24 +0900

    [add] タイトル画面のバックスクリーンを追加

--- Assets/Sources/UI/MainMenuController.cs
+++ Assets/Sources/UI/MainMenuController.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MainMenuController : MonoBehaviour {
+    GameObject bgImage;
 
+    private int frame = 0;
 	// Use this for initialization
 	void Start () {
-		
+        bgImage = GameObject.Find("bgImage");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        bgImage.transform.Rotate(0f, 0f, 0.05f);
 	}
 }
