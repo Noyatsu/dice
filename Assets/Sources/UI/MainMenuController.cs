cf5bf61df2abdff4c56489cb24aa703a2e24e304
witalosk <shun_september@live.jp>
2019-04-21 19:59:47 +0900

    [fix] チュートリアルがおかしい問題

--- Assets/Sources/UI/MainMenuController.cs
+++ Assets/Sources/UI/MainMenuController.cs
@@ -2,18 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MainMenuController : MonoBehaviour {
+public class MainMenuController : MonoBehaviour
+{
     GameObject bgImage;
 
     private int frame = 0;
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         bgImage = GameObject.Find("bgImage");
         BgmManager.Instance.Play("opening");
     }
-	
-	// Update is called once per frame
-	void Update () {
-        bgImage.transform.Rotate(0f, 0f, 0.05f);
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgImage.transform.Rotate(0f, 0f, Time.deltaTime * 2.0f);
+    }
 }
