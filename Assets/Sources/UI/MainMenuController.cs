399fe3c2311471023d06c7a802b09f7abd2b2d2b
unknown <shun_september@live.jp>
2019-05-01 16:22:00 +0900

    [add] ランク機能

--- Assets/Sources/UI/MainMenuController.cs
+++ Assets/Sources/UI/MainMenuController.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour
 {
     GameObject bgImage;
-    [SerializeField] GameObject StageDice, Up, Down, Left, Right;
+    [SerializeField] GameObject StageDice, Up, Down, Left, Right, rankImg;
     int StageNum = 1;
     private int frame = 0;
     // Use this for initialization
@@ -23,8 +24,12 @@ public class MainMenuController : MonoBehaviour
         else
         {
             BgmManager.Instance.Play("opening");
-
         }
+
+        //ランクを表示
+        Texture2D texture = Resources.Load("ranks/" + getString()) as Texture2D;
+        rankImg.GetComponent<Image>().sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+
     }
 
     // Update is called once per frame
@@ -37,4 +42,22 @@ public class MainMenuController : MonoBehaviour
     {
         naichilab.RankingLoader.Instance.ShowRanking();
     }
+    string getString()
+    {
+        int i = PlayerPrefs.GetInt("rank", 0) / 100;
+
+        if (i < 1) return "C-";
+        else if (i < 2) return "C+";
+        else if (i < 3) return "B-";
+        else if (i < 4) return "B+";
+        else if (i < 5) return "A-";
+        else if (i < 6) return "A+";
+        else if (i < 7) return "S-";
+        else if (i < 8) return "S+";
+        else if (i < 9) return "Master";
+        else return "Grand-Master";
+    }
 }
+
+
+ 
