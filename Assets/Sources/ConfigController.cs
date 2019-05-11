e96ba16cc3d42f53cdbb4246b5b60e0b32ad4410
Kosuke Sasaki <sasaki-trombone@users.noreply.github.com>
2019-05-11 16:34:23 +0900

    [add] configメニューにBGM変更スライダーを追加

---
+++ Assets/Sources/ConfigController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConfigController : MonoBehaviour
+{
+    Text bgmText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgmText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void volumeUpdate (float bgmValue)
+    {
+        BgmManager.Instance.TargetVolume = bgmValue;
+        BgmManager.Instance.CurrentAudioSource.volume = bgmValue;
+        bgmText.text = Mathf.RoundToInt(bgmValue*100).ToString();
+    }
+}
